6ee11c4fc6c8719bd34d53ce64ca39b1f7a4525a
TuanNghiaDo <dotuannghia07@gmail.com>
2025-07-30 22:31:26 +0700

    Feat: update SidebarProductItem to use new CartIconStroked and adjust icon sizes

--- e-commerce/src/components/SidebarProductItem/SidebarProductItem.jsx
+++ e-commerce/src/components/SidebarProductItem/SidebarProductItem.jsx
@@ -1,37 +1,39 @@
+
 import styles from './SidebarProductItem.module.scss';
-import { EyeIcon, CartIcon, SyncIcon, HeartIcon } from '@components/Icons/Icons';
+import { EyeIcon, CartIconStroked, SyncIcon, HeartIcon } from '@components/Icons/Icons';
 function SidebarProductItem() {
     return (
         <div className={styles.wrapper}>
             <button className={styles.btn}>
-                <CartIcon
+                <CartIconStroked
                     className={styles.icon}
                     width="2.6rem"
                     height="2.6rem"
-
+                    strokeWidth="0.5"
                 />
             </button>
             <button className={styles.btn}>
                 <HeartIcon
                     className={styles.icon}
-                    width="2.6rem"
-                    height="2.6rem"
+                    width="2.8rem"
+                    height="2.8rem"
+                    strokeWidth="0.3"
 
                 />
             </button>
             <button className={styles.btn}>
                 <SyncIcon
                     className={styles.icon}
-                    width="2.6rem"
-                    height="2.6rem"
+                    width="2.4rem"
+                    height="2.4rem"
 
                 />
             </button>
             <button className={styles.btn}>
                 <EyeIcon
                     className={styles.icon}
-                    width="2.4rem"
-                    height="2.4rem"
+                    width="3.0rem"
+                    height="3.0rem"
                 />
             </button>
         </div>
