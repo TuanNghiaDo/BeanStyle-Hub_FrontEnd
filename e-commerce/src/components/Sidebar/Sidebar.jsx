390b42534c28ea7e0420949e1672f109ec7e0117
TuanNghiaDo <dotuannghia07@gmail.com>
2025-08-04 15:18:43 +0700

    Feat: add CloseIcon component and integrate it into Sidebar for improved user experience

--- e-commerce/src/components/Sidebar/Sidebar.jsx
+++ e-commerce/src/components/Sidebar/Sidebar.jsx
@@ -2,13 +2,24 @@ import clsx from 'clsx'
 import styles from './Sidebar.module.scss'
 import { useContext } from 'react'
 import { SidebarContext } from '@contexts/SidebarProvider'
+import { CloseIcon } from '@icons/Icons'
 function Sidebar() {
 
     const { isOpen, setIsOpen } = useContext(SidebarContext)
+
+    const handleToggle = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <div className={styles.wrapper}>
-            <div className={clsx({ [styles.overlay]: isOpen })}></div>
+            <div
+                className={clsx({ [styles.overlay]: isOpen })}
+                onClick={handleToggle}></div>
             <div className={clsx(styles.sidebar, { [styles.slideSidebar]: isOpen })}>
+                {isOpen && <div className={styles.closeIcon} onClick={handleToggle}>
+                    <CloseIcon />
+                </div>}
                 sidebar
             </div>
         </div >
