9b6ba6334c58b530807c49f7f9c5c2dd09ee0212
TuanNghiaDo <dotuannghia07@gmail.com>
2025-09-03 22:22:56 +0700

    Feat: enhance CartSummary and CartTable styles; update Content layout for improved responsiveness

--- e-commerce/src/pages/Cart/components/contents/CartSummary.jsx
+++ e-commerce/src/pages/Cart/components/contents/CartSummary.jsx
@@ -2,22 +2,50 @@ import clsx from 'clsx';
 import styles from './CartSummary.module.scss';
 import Button from '@components/Button/Button'
 function CartSummary() {
+
+    const methodImgs = [
+        "https://xstore.8theme.com/elementor2/marseille04/wp-content/themes/xstore/images/woocommerce/payment-icons/visa.jpeg",
+        "https://xstore.8theme.com/elementor2/marseille04/wp-content/themes/xstore/images/woocommerce/payment-icons/master-card.jpeg",
+        "https://xstore.8theme.com/elementor2/marseille04/wp-content/themes/xstore/images/woocommerce/payment-icons/paypal.jpeg",
+        "https://xstore.8theme.com/elementor2/marseille04/wp-content/themes/xstore/images/woocommerce/payment-icons/american-express.jpeg",
+        "https://xstore.8theme.com/elementor2/marseille04/wp-content/themes/xstore/images/woocommerce/payment-icons/maestro.jpeg",
+    ];
+
     return (
         <div className={styles.wrapper}>
-            <span>Cart Total</span>
-            <hr />
-            <div className={clsx(styles.wrapperPrice, styles.subtotal)}>
-                <span>Subtotal</span>
-                <span>$1.9999</span>
-            </div>
-            <div className={clsx(styles.wrapperPrice, styles.total)}>
-                <span>Total</span>
-                <span>$1.9999</span>
-            </div>
+            <div className={styles.wrapperInfoSummary}>
+                <span>Cart Total</span>
+                <hr />
+                <div className={clsx(styles.wrapperPrice, styles.subtotal)}>
+                    <span>Subtotal</span>
+                    <span>$1.9999</span>
+                </div>
+                <div className={clsx(styles.wrapperPrice, styles.total)}>
+                    <span>Total</span>
+                    <span>$1.9999</span>
+                </div>
 
-            <div className={styles.wrapperButton}>
-                <Button className={styles.btn}>Tiến hành thanh toán</Button>
-                <Button className={styles.btn} secondary>Tiếp tục mua sắm</Button>
+                <div className={styles.wrapperButton}>
+                    <Button className={styles.btn}>Tiến hành thanh toán</Button>
+                    <Button className={styles.btn} secondary>Tiếp tục mua sắm</Button>
+                </div>
+            </div>
+            <div className={styles.methodPayment}>
+                <div className={styles.titleMethod}>
+                    Đảm bảo thanh toán <span>an toàn</span>
+                </div>
+                <div className={styles.imgMethodWrapper}>
+                    {methodImgs.map((img, index) => {
+                        return (
+                            <img
+                                key={index}
+                                src={img}
+                                alt="Payment Methods"
+                            />
+                        );
+                    })}
+                </div>
+                <span>Thanh toán của bạn <strong>an toàn 100%</strong></span>
             </div>
         </div>
     );
