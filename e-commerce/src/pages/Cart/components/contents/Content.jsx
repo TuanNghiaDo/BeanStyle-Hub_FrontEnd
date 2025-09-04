77b0ed7965714fcd7f8c00317bd8b27fb08483e3
TuanNghiaDo <dotuannghia07@gmail.com>
2025-09-04 16:12:21 +0700

    Feat: add coupon input and delete all button to Cart; enhance styles for cart footer

--- e-commerce/src/pages/Cart/components/contents/Content.jsx
+++ e-commerce/src/pages/Cart/components/contents/Content.jsx
@@ -2,11 +2,39 @@
 import styles from './Content.module.scss';
 import CartTable from './CartTable.jsx';
 import CartSummary from './CartSummary.jsx';
+import Button from '@components/Button/Button.jsx';
+import { DeleteIcon } from '@components/icons/Icons';
 function Content() {
     return (
         <div className={styles.wrapper}>
             <div className={styles.cartTable}>
                 <CartTable />
+                <div className={styles.cartFooter}>
+                    <div className={styles.couponWrapper}>
+                        <input
+                            type="text"
+                            placeholder="Nhập mã giảm giá ..."
+                            className={styles.couponInput}
+                        />
+                        <Button
+                            secondary
+                            className={styles.couponBtn}
+                        >
+                            Áp dụng
+                        </Button>
+                    </div>
+                    <div className={styles.deleteAllWrapper}>
+                        <Button
+                            secondary
+                            className={styles.deleteBtn}
+                        >
+                            <DeleteIcon
+                                className={styles.deleteIcon}
+                            />
+                            <span>Xóa toàn bộ sản phẩm khỏi giỏ hàng</span>
+                        </Button>
+                    </div>
+                </div>
             </div>
 
             <div className={styles.cartSummary}>
